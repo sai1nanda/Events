a947ee19bce7cba008abc6d373352324ded31f8a
sai1nanda <69192974+sai1nanda@users.noreply.github.com>
2022-01-16 14:39:59 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+
+app.get("/", function(req, res){
+    res.render("index");
+  });
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
